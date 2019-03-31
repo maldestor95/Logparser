61899b8dafc8558b5afd334e4cbc174999f13324
maldestor <maldestor95@gmail.com>
2019-03-31 22:51:06 +0200

    editbox

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget , QLabel , QToolTip , QPushButton , QMessageBox
-from PyQt5.QtGui import QIcon, QFont
- 
+from PyQt5.QtWidgets import QApplication, QWidget , QLabel , QToolTip , QPushButton , QMessageBox, QTextEdit, QFormLayout, QLineEdit,QInputDialog
+from PyQt5.QtGui import QIcon, QFont, QTextDocument
+import os
+
 class App(QWidget):
  
     def __init__(self):
@@ -12,45 +13,65 @@ class App(QWidget):
         self.top = 50
         self.width = 640
         self.height = 480
+        self.openlog('t.log')
         self.initUI()
-     
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        # self.se
 
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setToolTip('This is a <b>QWidget</b> widget')
         
-        btn = QPushButton('Button', self)
-        # QPushButton(string text, QWidget parent = None)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)  
-             
-        
-        qbtn = QPushButton('Quit', self)
-        qbtn.clicked.connect(QApplication.instance().quit)
-        qbtn.resize(qbtn.sizeHint())
-        qbtn.move(100, 50)
+        # self.SourceDoc=QTextDocument("")  
+        self.qTE=QTextEdit(self)
+        self.qTE.acceptDrops=True
+        self.qTE.setGeometry(0, 0, self.frameGeometry().width(), self.frameGeometry().height()-100)
+        self.qTE.setDocument(self.SourceDoc)
+        self.qTE.show()
 
-        self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle('Tooltips')    
         self.show()
 
-
         self.show()
-
+        self.IDD=inputdialogdemo(self)
+        self.IDD.show()
+        
     def closeEvent(self, event):
         
-        reply = QMessageBox.question(self, 'Message',
-            "Are you sure to quit?", QMessageBox.Yes | 
-            QMessageBox.No, QMessageBox.No)
+        # reply = QMessageBox.question(self, 'Message',
+        #     "Are you sure to quit?", QMessageBox.Yes | 
+        #     QMessageBox.No, QMessageBox.No)
 
-        if reply == QMessageBox.Yes:
+        # if reply == QMessageBox.Yes:
             event.accept()
-        else:
-            event.ignore()
+        # else:
+        #     event.ignore()
+    def openlog(self,File_Path):
+        if os.path.isfile(File_Path):
+            Fr=open(File_Path,"r")
+            self.SourceDoc=QTextDocument(Fr.read())
+
+class inputdialogdemo(QWidget):
+   def __init__(self, parent = App):
+      super(inputdialogdemo, self).__init__(parent)
+        
+      layout = QFormLayout()
+
+      self.le = QLineEdit()
+      self.btn = QPushButton("Filter")
+      self.btn.clicked.connect(self.getFilter)  
+      layout.addRow(self.le,self.btn)
+        
+   def getFilter(self):
+      items = ("C", "C++", "Java", "Python")
+        
+      item, ok = QInputDialog.getItem(self, "select input dialog", 
+         "list of languages", items, 0, False)
+            
+      if ok and item:
+         self.le.setText(item)
+        
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
