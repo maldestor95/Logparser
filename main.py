02b788d257378492f39cbd45d2a674ed748b0ab1
Maldestor95 <maldestor95@gmail.com>
2019-04-01 11:54:03 +0200

    1st Filter done

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget , QLabel , QToolTip , QPushButton , QMessageBox, QTextEdit, QGridLayout, QLineEdit,QInputDialog
-from PyQt5.QtGui import QIcon, QFont, QTextDocument
-import os
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QIcon, QFont, QTextDocument, QTextCursor
+import os,re
 
 class App(QWidget):
  
@@ -30,10 +31,13 @@ class App(QWidget):
         self.btn = QPushButton("Filter")
         self.btn.clicked.connect(self.getFilter)  
         layout.addWidget(self.le,0,0)
-        layout.addWidget(self.btn,1,0)
+        layout.addWidget(self.btn,0,1)
           
         self.qTE=QTextEdit(self)
-        layout.addWidget(self.qTE,0,0,5,2)  
+        layout.addWidget(self.qTE,1,0,3,2)  
+        
+        self.qTEout=QTextEdit(self)
+        layout.addWidget(self.qTEout,4,0,3,2)  
 
         self.setLayout(layout)
         self.qTE.acceptDrops=True
@@ -45,16 +49,30 @@ class App(QWidget):
 
         
     def getFilter(self):
-      print(self.le.text())
       tt=QTextDocument(self.le.text())
-      self.qTE.setDocument(tt)
-        # items = ("C", "C++", "Java", "Python")
-          
-      # item, ok = QInputDialog.getItem(self, "select input dialog", 
-      #   "list of languages", items, 0, False)
-              
-      # if ok and item:
-      #   self.le.setText(item)
+      # lRe=QRegExp("^((?!look).)*$")#self.le.text())
+      # print("^((?!{0}).)*$".format(tt.toPlainText()))
+      lRe=QRegExp("^((?!{0}).)*$".format(tt.toPlainText()))#self.le.text())
+      lDoc=QTextDocument(self.qTE.document().toPlainText())
+
+      # ^.+?(?=look)  jusqu'au mot look
+      # ^((?!look).)*$   lines not containing look
+
+      lCursor=lDoc.find(lRe)
+      EndDocCursor=QTextCursor(lDoc)
+      EndDocCursor.movePosition(QTextCursor.End)
+      EndDocCursorPos=EndDocCursor.position()
+      # print(lCursor.position(), EndDocCursorPos)
+
+      while lCursor.position()< EndDocCursorPos :
+        lCursor.movePosition(QTextCursor.NextBlock,QTextCursor.KeepAnchor)
+        lCursor.removeSelectedText();
+        lCursor=lDoc.find(lRe)
+        EndDocCursor.movePosition(QTextCursor.End)
+        EndDocCursorPos=EndDocCursor.position()
+        # print(lCursor.position(), EndDocCursorPos)
+
+      self.qTEout.setDocument(lDoc)
     def closeEvent(self, event):
         
         # reply = QMessageBox.question(self, 'Message',
